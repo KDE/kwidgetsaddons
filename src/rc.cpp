242c539bf3c79a576f56edc23013b6ce713e8080
l10n daemon script <scripty@kde.org>
2014-03-27 01:44:43 +0000

    SVN_SILENT made messages (after extraction)

---
+++ src/rc.cpp
@@ -0,0 +1,33 @@
+// i18n: file: knewpassworddialog.ui:52
+// i18n: ectx: property (text), widget (QLabel, labelPassword)
+i18n("Password:");
+// i18n: file: knewpassworddialog.ui:62
+// i18n: ectx: property (text), widget (QLabel, label_2)
+i18n("&Verify:");
+// i18n: file: knewpassworddialog.ui:90
+// i18n: ectx: property (text), widget (QLabel, labelStrengthMeter)
+i18n("Password strength meter:");
+// i18n: file: knewpassworddialog.ui:112
+// i18n: ectx: property (text), widget (KTitleWidget, labelMatch)
+i18n("Passwords do not match");
+// i18n: file: kpassworddialog.ui:25
+// i18n: ectx: property (text), widget (QLabel, prompt)
+i18n("Supply a username and password below.");
+// i18n: file: kpassworddialog.ui:62
+// i18n: ectx: property (text), widget (QRadioButton, anonymousRadioButton)
+i18n("No password, use anonymous (or guest) login");
+// i18n: file: kpassworddialog.ui:69
+// i18n: ectx: property (text), widget (QRadioButton, usePasswordButton)
+i18n("Use this password:");
+// i18n: file: kpassworddialog.ui:85
+// i18n: ectx: property (text), widget (QLabel, userNameLabel)
+i18n("Username:");
+// i18n: file: kpassworddialog.ui:99
+// i18n: ectx: property (text), widget (QLabel, domainLabel)
+i18n("Domain:");
+// i18n: file: kpassworddialog.ui:113
+// i18n: ectx: property (text), widget (QLabel, passwordLabel)
+i18n("Password:");
+// i18n: file: kpassworddialog.ui:130
+// i18n: ectx: property (text), widget (QCheckBox, keepCheckBox)
+i18n("Remember password");
