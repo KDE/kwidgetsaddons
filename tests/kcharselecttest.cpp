7d0578ada8c5c98f2c7b879ff362bcf2e9093f36
Christoph Feck <cfeck@kde.org>
2017-03-17 17:33:22 +0100

    Enable all planes in kcharselect test dialog

--- tests/kcharselecttest.cpp
+++ tests/kcharselecttest.cpp
@@ -11,6 +11,7 @@ int main(int argc, char **argv)
 
 
     KCharSelect selector(nullptr, nullptr);
+    selector.setAllPlanesEnabled(true);
     selector.resize(selector.sizeHint());
     selector.show();
     selector.setWindowTitle(QStringLiteral("KCharSelect Test"));
