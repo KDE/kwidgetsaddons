ccaf6fe3b871a64d0f785efa7338ee68926c1bd6
Christoph Feck <cfeck@kde.org>
2020-04-02 11:01:43 +0200

    Convert #warning to #pragma message GIT_SILENT

--- src/kfontaction.cpp
+++ src/kfontaction.cpp
@@ -142,9 +142,7 @@ QString KFontAction::font() const
 QWidget *KFontAction::createWidget(QWidget *parent)
 {
 //    qCDebug(KWidgetsAddonsLog) << "KFontAction::createWidget()";
-#ifdef __GNUC__
-#warning FIXME: items need to be converted
-#endif
+#pragma message "FIXME: items need to be converted"
     // This is the visual element on the screen.  This method overrides
     // the KSelectAction one, preventing KSelectAction from creating its
     // regular KComboBox.
