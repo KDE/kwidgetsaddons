b40e63b73036d9602dbafe038fdfca3d0c0bc304
Andreas Cord-Landwehr <cordlandwehr@kde.org>
2021-06-20 11:45:50 +0200

    Fix SPDX syntax error

--- src/kpixmapregionselectorwidget.cpp
+++ src/kpixmapregionselectorwidget.cpp
@@ -2,7 +2,7 @@
     This file is part of the KDE libraries
     SPDX-FileCopyrightText: 2004 Antonio Larrosa <larrosa@kde.org>
 
-    SPDX-License-Identifier: LGPL-2.0-or-later>
+    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 
 #include "kpixmapregionselectorwidget.h"
