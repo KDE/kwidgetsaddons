cc112637992015f7507a6ac92008fd45789cbd12
Ahmad Samir <a.samirh78@gmail.com>
2021-09-04 20:36:34 +0200

    Tweak the API docs
    GIT_SILENT

--- src/kmodifierkeystatus.h
+++ src/kmodifierkeystatus.h
@@ -21,16 +21,16 @@ class KModifierKeyStatusProvider;
  *
  * This class provides cross-platform information about the state of the
  * keyboard's modifier keys and the mouse buttons and allows to change the
- * state as well.
+ * states as well.
  *
  * It recognizes two states a key can be in:
- * @li @em locked: eg. caps-locked (a.k.a. toggled)
- * @li @em latched the key is temporarily locked but will be unlocked upon
- *                          the next keypress.
+ * @li @em locked: e.g. caps-locked (a.k.a. toggled)
+ * @li @em latched: the key is temporarily locked but will be unlocked upon
+ * the next keypress.
  *
  * An application can either query the states synchronously (isKeyLatched(),
- * isKeyLocked()) or connect to KModifierKeyStatus's signals to be notified about
- * changes (keyLatched(), keyLocked()).
+ * isKeyLocked()) or connect to KModifierKeyStatus's signals (e.g. keyLatched(),
+ * keyLocked()) to be notified about changes.
  *
  * @since 5.87
  */
@@ -52,8 +52,8 @@ public:
     /**
      * Check if a key is known by the underlying window system and can be queried.
      *
-     * @param key The key to check
-     * @return true if the key is available, false if it is unknown
+     * @param key the key to check
+     * @return @c true if the key is available, @c false if it is unknown
      */
     bool knowsKey(Qt::Key key) const;
 
@@ -67,108 +67,111 @@ public:
     /**
      * Synchronously check if a key is pressed.
      *
-     * @param key The key to check
-     * @return true if the key is pressed, false if the key is not pressed or unknown.
-     * @see isKeyLatched, @see isKeyLocked, @see keyPressed
+     * @param key the key to check
+     * @return @c true if the key is pressed, @c false if the key is not pressed
+     * or unknown.
+     *
+     * @see isKeyLatched, isKeyLocked, keyPressed
      */
     bool isKeyPressed(Qt::Key key) const;
 
     /**
      * Synchronously check if a key is latched.
      *
-     * @param key The key to check
-     * @return true if the key is latched, false if the key is not latched or unknown.
-     * @see isKeyPressed, @see isKeyLocked, @see keyLatched
+     * @param key the key to check
+     * @return @c true if the key is latched, @c false if the key is not latched or unknown.
+     *
+     * @see isKeyPressed, isKeyLocked, keyLatched
      */
     bool isKeyLatched(Qt::Key key) const;
 
     /**
      * Set the latched state of a key.
      *
-     * @param key The key to latch
-     * @param latched true to latch the key, false to unlatch it.
-     * @return false if the key is unknown. true doesn't guarantee you the
-     *         operation worked.
+     * @param key the key to latch
+     * @param latched @c true to latch the key, @c false to unlatch it.
+     * @return @c true if the request was sent to the underlying system
+     * (doesn't guarantee that the key state was actually changed), otherwise
+     * returns @c false if the key is unknown.
      */
     bool setKeyLatched(Qt::Key key, bool latched);
 
     /**
      * Synchronously check if a key is locked.
      *
-     * @param key The key to check
-     * @return true if the key is locked, false if the key is not locked or unknown.
-     * @see isKeyPressed, @see isKeyLatched, @see keyLocked
+     * @param key the key to check
+     * @return @c true if the key is locked, @c false if the key is not locked or unknown.
+     * @see isKeyPressed(), isKeyLatched(), keyLocked()
      */
     bool isKeyLocked(Qt::Key key) const;
 
     /**
      * Set the locked state of a key.
      *
-     * @param key The key to lock
-     * @param latched true to lock the key, false to unlock it.
-     * @return false if the key is unknown. true doesn't guarantee you the
-     *         operation worked.
+     * @param key the key to lock
+     * @param latched @c true to lock the key, @c false to unlock it.
+     * @return @c true if the request was sent to the underlying system
+     * (doesn't guarantee that the key state was actually changed), otherwise
+     * returns @c false if the key is unknown.
      */
     bool setKeyLocked(Qt::Key key, bool locked);
 
     /**
      * Synchronously check if a mouse button is pressed.
      *
-     * @param button The mouse button to check
-     * @return true if the mouse button is pressed, false if the mouse button
-     *         is not pressed or its state is unknown.
+     * @param button the mouse button to check
+     * @return @c true if the mouse button is pressed, @c false if the mouse button
+     * is not pressed or its state is unknown.
      */
     bool isButtonPressed(Qt::MouseButton button) const;
 
 Q_SIGNALS:
     /**
-     * This signal is emitted whenever the pressed state of a key changes
-     * (key press or key release).
+     * Emitted whenever the pressed state of a key changes (key press or key release).
      *
-     * @param key The key that changed state
-     * @param pressed true if the key is now pressed, false if is released.
+     * @param key the key that changed state
+     * @param pressed @c true if the key is now pressed, @c false if is released
      */
     void keyPressed(Qt::Key key, bool pressed);
 
     /**
-     * This signal is emitted whenever the latched state of a key changes.
+     * Emitted whenever the latched state of a key changes.
      *
-     * @param key The key that changed state
-     * @param latched true if the key is now latched, false if it isn't
+     * @param key the key that changed state
+     * @param latched @c true if the key is now latched, @c false if it isn't
      */
     void keyLatched(Qt::Key key, bool latched);
 
     /**
-     * This signal is emitted whenever the locked state of a key changes.
+     * Emitted whenever the locked state of a key changes.
      *
-     * @param key The key that changed state
-     * @param locked true if the key is now locked, false if it isn't
+     * @param key the key that changed state
+     * @param locked @c true if the key is now locked, @c false if it isn't
      */
     void keyLocked(Qt::Key key, bool locked);
 
     /**
-     * This signal is emitted whenever the pressed state of a mouse button
-     * changes (mouse button press or release).
+     * Emitted whenever the pressed state of a mouse button changes (mouse
+     * button press or release).
      *
-     * @param button The mouse button that changed state
-     * @param pressed true if the mouse button is now pressed, false if
-     *                is released.
+     * @param button the mouse button that changed state
+     * @param pressed @c true if the mouse button is now pressed, @c false if
+     * it is released
      */
     void buttonPressed(Qt::MouseButton button, bool pressed);
 
     /**
-     * This signal is emitted whenever a new modifier is found due to
-     * the keyboard mapping changing.
+     * Emitted whenever a new modifier is found due to the keyboard mapping changing.
      *
-     * @param key The key that was discovered
+     * @param key the key that was discovered
      */
     void keyAdded(Qt::Key key);
 
     /**
-     * This signal is emitted whenever a previously known modifier no
-     * longer exists due to the keyboard mapping changing.
+     * Emitted whenever a previously known modifier no longer exists due to the
+     * keyboard mapping changing.
      *
-     * @param key The key that vanished
+     * @param key the key that vanished
      */
     void keyRemoved(Qt::Key key);
 
