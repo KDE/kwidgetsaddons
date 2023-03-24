46ae36fbb21d64d90c44e8d3ee2ec554f1fd832d
Friedrich W. H. Kossebau <kossebau@kde.org>
2023-03-24 18:52:05 +0100

    Mark private methods of public classes as unexported
    Given the methods are marked as private and thus not intended for
    external use, exporting the symbols adds some minor unneeded costs
    in binary size and linking time, which can be avoided.
    
    NO_CHANGELOG

--- src/kmultitabbar.h
+++ src/kmultitabbar.h
@@ -227,17 +227,18 @@ private:
     KMultiTabBar::KMultiTabBarPosition m_position;
     KMultiTabBar::KMultiTabBarStyle m_style;
 
-    void computeMargins(int *hMargin, int *vMargin) const;
-    QSize computeSizeHint(bool withText) const;
-    bool shouldDrawText() const;
-    bool isVertical() const;
+    KWIDGETSADDONS_EXPORT void computeMargins(int *hMargin, int *vMargin) const;
+    KWIDGETSADDONS_EXPORT QSize computeSizeHint(bool withText) const;
+    KWIDGETSADDONS_EXPORT bool shouldDrawText() const;
+    KWIDGETSADDONS_EXPORT bool isVertical() const;
 
-    void initStyleOption(QStyleOptionToolButton *opt) const;
+    KWIDGETSADDONS_EXPORT void initStyleOption(QStyleOptionToolButton *opt) const;
 
     friend class KMultiTabBarInternal;
     /**
      * This class should never be created except with the appendTab call of KMultiTabBar
      */
+    KWIDGETSADDONS_EXPORT
     KMultiTabBarTab(const QIcon &icon, const QString &, int id, QWidget *parent, KMultiTabBar::KMultiTabBarPosition pos, KMultiTabBar::KMultiTabBarStyle style);
 
     std::unique_ptr<class KMultiTabBarTabPrivate> const d;
