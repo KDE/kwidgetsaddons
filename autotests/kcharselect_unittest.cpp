6482c60955c442fa31d6ea82a95f02c594523545
Albert Astals Cid <aacid@kde.org>
2023-03-28 19:35:58 +0000

    Add test for KCharSelect bug not doing anything when entering 2 chars

--- autotests/kcharselect_unittest.cpp
+++ autotests/kcharselect_unittest.cpp
@@ -7,6 +7,7 @@
 #include <kcharselect.h>
 
 #include <QComboBox>
+#include <QLineEdit>
 #include <QTest>
 
 class KCharSelectTest : public QObject
@@ -32,6 +33,16 @@ private Q_SLOTS:
         QVERIFY(blockCombo);
         blockCombo->setCurrentIndex(1);
         QCOMPARE(selector.currentCodePoint(), 128);
+    }
+
+    void search2Chars()
+    {
+        KCharSelect selector(nullptr, nullptr);
+        QLineEdit *searchLineEdit = selector.findChild<QLineEdit *>();
+        QVERIFY(searchLineEdit);
+        searchLineEdit->setText(QStringLiteral("pi"));
+        Q_EMIT searchLineEdit->returnPressed();
+        QVERIFY(selector.displayedChars().contains(QChar(960))); // 960 == π
     }
 };
 
